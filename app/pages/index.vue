b328604bf47cf9d857cb0427a4c5113675ae73bf
Aife Portable <clapette01@gmail.com>
2025-08-29 11:00:26 +0200

    u

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,36 +2,14 @@
 import ThreeViewport from '~/features/three/ThreeViewport.vue'
 
 definePageMeta({
-  layout: 'home',
+  layout: 'default',
 })
 
 const online = useOnline()
 </script>
 
 <template>
-  <div>
-    <Logos mb-6 />
     <ClientOnly>
-      <Suspense>
-        <PageView v-if="online" />
-        <div v-else text-gray:80>
-          You're offline
-        </div>
-        <template #fallback>
-          <div op50 italic>
-            <span animate-pulse>Loading...</span>
-          </div>
-        </template>
-      </Suspense>
-      <template #fallback>
-        <div op50>
-          <span animate-pulse>...</span>
-        </div>
-      </template>
+      <ThreeViewport class="h-full w-full" />
     </ClientOnly>
-    <InputEntry />
-    <ClientOnly>
-      <ThreeViewport class="mt-8 h-80 w-full" />
-    </ClientOnly>
-  </div>
 </template>
